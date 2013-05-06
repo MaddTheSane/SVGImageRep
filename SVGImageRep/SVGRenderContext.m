9126f09dde54531e416b4632d7546baf82a9adad
C.W. Betts <computers57@hotmail.com>
2013-05-06 11:47:29 -0600

    Use an autorelease block instead of a class.

--- SVGImageRep/SVGRenderContext.m
+++ SVGImageRep/SVGRenderContext.m
@@ -1230,13 +1230,10 @@ static svg_status_t r_render_text(void *closure, svg_length_t *x, svg_length_t *
 {
 	SVGRenderContext *self = (SVGRenderContext *)closure;
 	//CGContextRef CGCtx = CGLayerGetContext(self.renderLayer);
-	svg_status_t retStat = 0;
-	{
-		NSAutoreleasePool *pool = [NSAutoreleasePool new];
-		retStat = [self renderText:utf8 atX:[self lengthToPoints:x] y:[self lengthToPoints:y]];
-		[pool drain];
+
+	@autoreleasepool {
+		return [self renderText:utf8 atX:[self lengthToPoints:x] y:[self lengthToPoints:y]];
 	}
-	return retStat;
 }
 
 const svg_render_engine_t cocoa_svg_engine =
