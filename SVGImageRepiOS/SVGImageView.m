074198857e433748e3797a010eb7f008e5aee884
C.W. Betts <computers57@hotmail.com>
2013-05-16 00:12:13 -0600

    Clear the SVGPrivate variable when we free the data.

--- SVGImageRepiOS/SVGImageView.m
+++ SVGImageRepiOS/SVGImageView.m
@@ -79,6 +79,7 @@
 {
 	if (svgPrivate) {
 		svg_destroy(svgPrivate);
+		svgPrivate = NULL;
 	}
 	svg_create((svg_t **)&svgPrivate);
 	svg_status_t status = svg_parse_buffer(svgPrivate, [data bytes], [data length]);
@@ -94,6 +95,7 @@
 {
 	if (svgPrivate) {
 		svg_destroy(svgPrivate);
+		svgPrivate = NULL;
 	}
 	svg_create((svg_t **)&svgPrivate);
 	svg_status_t status = svg_parse(svgPrivate, [path fileSystemRepresentation]);
